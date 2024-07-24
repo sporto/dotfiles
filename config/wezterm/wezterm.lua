25e6664a25f9d53673904e13da550637d256d9d9
Sebastian Porto <s@porto5.com>
2024-07-25 09:07:18 +1000

    Use theme

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -7,6 +7,7 @@ local config = wezterm.config_builder()
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 30
 config.window_decorations = "RESIZE"
+config.color_scheme = 'Ayu Mirage'
 
 config.font =
     wezterm.font('JetBrains Mono', {})
